9efed63a45d460332f6ec136bfb663bd8afddac4
Your Name <csapo.tamas@diak.szi-pg.hu>
2025-11-09 20:53:38 +0100

    törlés

--- script.js
+++
@@ -1,7 +0,0 @@
-function loadPage(page, id) {
-            fetch(page)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById(id).innerHTML = data;
-            });
-        }
