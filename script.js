ac03d692deb71566b9f6085d4d906143fbb951a7
Szabo Istvan <pisti4395@gmail.com>
2025-11-09 23:59:33 +0100

    search probalkozas

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function add_to_cart(painting) {
   console.log(painting)
   fetch("/vetel/"+painting)
 }
+
+function search(name){
+  div = document.getElementById("content")
+}
+
+
+function highlight(searchTerm) {
+  // Remove old highlights
+  document.getElementById("content").innerHTML = document.getElementById("content").innerHTML.replace(
+    /<span class="highlight">(.*?)<\/span>/gi, '$1'
+  );
+
+  // Add new highlights
+  if (searchTerm) {
+    document.body.innerHTML = document.body.innerHTML.replace(
+      new RegExp(searchTerm, 'gi'), 
+      '<span class="highlight">$&</span>'
+    );
+  }
+}
+
