15afecf3ef3f88c15ca478846a684a3d6932ec65
Szabo Istvan <pisti4395@gmail.com>
2025-11-09 17:11:45 +0100

    orderbutton

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function loadPage(page, id) {
-            fetch(page)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById(id).innerHTML = data;
-            });
-        }
+  fetch(page)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+function add_to_cart(painting) {
+  console.log(painting)
+}
